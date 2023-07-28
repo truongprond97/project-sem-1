d8992a676b02a9b4333646e7d91d74c76297401c
Gnourt69 <truong012378939291@gmail.com>
2023-07-28 11:11:06 +0700

    commit

--- public/index.js
+++ public/index.js
@@ -131,3 +131,16 @@ app.listen(port, hostName, () => {
       `Server listen on port ${port}: ${"http://" + hostName + ":" + port} `,)
 })
 
+
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  // res.setHeader('Content-Type', 'text/html');
+  // res.end('<h1>Hello World</h1>');
+});
+
+server.listen(port,() => {
+  console.log(`Server running at port `+port);
+});
+
