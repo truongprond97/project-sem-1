620f4fdea4ded6b1da30bf19f7af7c2a2dccd7d0
Gnourt69 <truong012378939291@gmail.com>
2023-07-28 18:41:03 +0700

    commit

--- src/route/customer.js
+++ src/route/customer.js
@@ -2,13 +2,16 @@ const express = require("express")
 const router = express.Router()
 
 const {getMessage,postMessage} = require("../controllers/customer.controller");
+const {getProduct} = require("../controllers/product.controller");
 
 router.get("/", async (request, response) => {
   try {
     const messages = await getMessage()
+    const products = await getProduct()
 
     const options = {
-      messages: messages.data.content
+      messages: messages.data.content,
+      products: products.data.content
     }
     response.status(200).render('home', options)
   } catch (e) {
